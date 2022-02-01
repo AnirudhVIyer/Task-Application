2b6be0bc344f1002f1b24b6b07cbb2c7e19b7bad
Anirudh Iyer <anirudhiyer@Anirudhs-MacBook-Air.local>
2022-02-01 17:36:40 +0530

    removing comments index.js

--- src/index.js
+++ src/index.js
@@ -9,13 +9,6 @@ const TaskRouter = require("./routers/TaskRouter").router;
 const app = express();
 const port = process.env.PORT;
 
-// // always put middleware at top, or in middleware folder
-
-// app.use((req, res, next) => {
-//   console.log("express middleware");
-//   next();
-// });
-
 app.use(express.json()); // parses the incoming request
 // send in json form only
 
@@ -25,32 +18,3 @@ app.use(TaskRouter);
 app.listen(port, () => {
   console.log("connected to server");
 });
-
-// const bcryptjs = require("bcryptjs");
-
-// const myFuc = async () => {
-//   const token = jwt.sign({ _id: "token124" }, "randomsecret"); //func returns a token to a client
-//   console.log(token);
-
-//   const ver = jwt.verify(token, "randomsecret", { expiresIn: "7 days" });
-//   console.log(ver);
-// };
-
-// myFuc();
-
-// linkink entities
-const User = require("./models/user").User;
-const Task = require("./models/tasks").TaskModel;
-const myFunc = async () => {
-  //finding task using the owner propery
-  // const task = await Task.findById("61f444789523b370d96da42a");
-  // await task.populate("owner");
-  // // we are populating the owner field of task(which had owner id) with the complete document of that owner from users collection
-  // console.log(task.owner);
-
-  // finding task using user -- virtual relation established
-  const user = await User.findById("61f442f3a69b2b32ff429e4b");
-  await user.populate("tasksVirtual");
-  console.log(user.tasksVirtual);
-};
-// myFunc();
